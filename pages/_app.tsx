b3c0f3b4e590676c7b3fe608587a87d35fafd3dd
Oluwatomisin John Badmus <155178644+tomisin05@users.noreply.github.com>
2025-05-06 22:50:12 -0400

    added logo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+    
+  return(
+    <div>
+    <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <title>M.A.R.C.(LE) - Word Guessing Game</title>
+        <meta name="description" content="A strategic word guessing game with ratings" />
+        <link rel="icon" type="image/png" href="/Marcle.png" />   
+    </Head>
+     <Component {...pageProps} />
+
+     </div>
+  )}
 
 export default MyApp
